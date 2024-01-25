1c9b5ca402efb3712da667a9bb4415deb1de667b
smithymmij <114240428+smithymmij@users.noreply.github.com>
2024-01-25 14:15:11 -0300

    Sua mensagem de commit aqui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/shop';
+import ShopCategory from './Pages/shopcategory';
+import Product from './Pages/product';
+import Cart from './Pages/cart';
+import LoginSignup from './Pages/loginsignup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="men" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/product/:productId' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/loginsignup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loginsignup = () => {
+  return (
+    <div>loginsignup</div>
+  )
+}
+
+export default loginsignup
